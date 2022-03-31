eb4c4c59a509f6a93c05041095e87d6f86a36e03
Dallin Charbonneau <dallin.charbonneau3983@stu.mtec.edu>
2022-03-31 09:57:34 -0600

    Finished Unit 7

--- src/app/components/game-search/game-search.component.ts
+++ src/app/components/game-search/game-search.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/app/interfaces/game';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
-  selector: 'app-game-search',
-  templateUrl: './game-search.component.html',
-  styleUrls: ['./game-search.component.scss']
+    selector: 'app-game-search',
+    templateUrl: './game-search.component.html',
+    styleUrls: ['./game-search.component.scss']
 })
-export class GameSearchComponent implements OnInit {
+export class GameSearchComponent implements OnInit
+{
+    searchText: string = "";
 
-  constructor() { }
+    games: Game[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor(private gameService: GameService) { }
 
+    ngOnInit(): void
+    {
+    }
+
+    search()
+    {
+        this.gameService.searchByName(this.searchText).subscribe(games => this.games = games);
+    }
 }
